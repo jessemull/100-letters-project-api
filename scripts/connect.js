93e19879de9f90b22420a5abe42bb47cb2934ffc
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-02-26 19:58:13 -0800

    Set up tunnel and db scripts again

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const { spawn } = require('child_process');
 
-// Load environment variables
 const sshPrivateKeyPath = process.env.SSH_PRIVATE_KEY_PATH;
 const sshUser = process.env.SSH_USER;
 const sshHost = process.env.SSH_HOST;
@@ -10,14 +9,12 @@ const dbUser = process.env.DB_USER;
 const dbName = process.env.DB_NAME;
 const dbPassword = process.env.DB_PASSWORD;
 
-// Command to establish the SSH tunnel
 const sshCommand = `ssh -i ${sshPrivateKeyPath} -L 5432:${dbHost}:5432 ${sshUser}@${sshHost} -f -N`;
 
-// Establish the SSH tunnel
 console.log(`Establishing SSH tunnel with the command: ${sshCommand}`);
 
 const sshProcess = spawn(sshCommand, {
-  shell: true, // Ensure the command runs as a shell command
+  shell: true,
 });
 
 sshProcess.stdout.on('data', (data) => {
@@ -31,14 +28,13 @@ sshProcess.stderr.on('data', (data) => {
 sshProcess.on('close', (code) => {
   console.log(`SSH tunnel process exited with code ${code}`);
   
-  // Now that the tunnel is up, connect to the database
   const psqlCommand = `PGPASSWORD=${dbPassword} psql -h localhost -U ${dbUser} -d ${dbName} -p 5432`;
 
   console.log(`Connecting to PostgreSQL using: ${psqlCommand}`);
 
   const psqlProcess = spawn(psqlCommand, {
     shell: true,
-    stdio: 'inherit', // This allows the psql command to use the same terminal session for input/output
+    stdio: 'inherit',
   });
 
   psqlProcess.on('close', (code) => {
