6641bdac94656747711e0926850526f291b997b3
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-03-03 21:14:11 -0800

    trigger buidls

--- routes/deleteCorrespondence/index.ts
+++ routes/deleteCorrespondence/index.ts
@@ -4,7 +4,7 @@ export const handler: APIGatewayProxyHandler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Delete Correspondence - Update',
+      message: 'Delete Correspondence',
     }),
   };
 };
--- routes/getRecipientById/index.ts
+++ routes/getRecipientById/index.ts
@@ -4,7 +4,7 @@ export const handler: APIGatewayProxyHandler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Get Recipient by ID - Update',
+      message: 'Get Recipient by ID',
     }),
   };
 };
