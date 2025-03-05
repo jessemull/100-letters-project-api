175397f27b0b6cf209f650339a7e11bfb213cff8
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-03-04 17:32:09 -0800

    feat: first route and logger

---
+++ common/util/logger.ts
@@ -0,0 +1,25 @@
+import * as bunyan from 'bunyan';
+import * as BunyanCloudWatch from 'bunyan-cloudwatch';
+
+const logger = bunyan.createLogger({
+  name: 'one-hundred-letters-api-logger',
+  level: (process.env.LOG_LEVEL as bunyan.LogLevel) || 'info',
+  serializers: bunyan.stdSerializers,
+  streams: [
+    {
+      level: 'info',
+      stream: process.stdout,
+    },
+    {
+      level: 'error',
+      type: 'raw',
+      stream: new BunyanCloudWatch({
+        logGroupName: '/aws/lambda/one-hundred-letters-api-log-group',
+        logStreamName: 'one-hundred-letters-api-log-stream',
+        awsRegion: 'us-west-2',
+      }),
+    },
+  ],
+});
+
+export { logger };
--- routes/getRecipients/index.ts
+++ routes/getRecipients/index.ts
@@ -1,10 +1,26 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { logger, prisma } from '../../common/util';
 
 export const handler: APIGatewayProxyHandler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Get Recipients',
-    }),
-  };
+  try {
+    const recipients = await prisma.person.findMany({
+      select: {
+        id: true,
+        name: true,
+        address: true,
+      },
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: recipients,
+      }),
+    };
+  } catch (error) {
+    logger.error('Error fetching recipients: ', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' }),
+    };
+  }
 };
