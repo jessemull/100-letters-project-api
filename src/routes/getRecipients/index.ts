6b2844b64a93819a6dfe077c8da614b665c3b725
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-03-06 13:58:52 -0800

    fix: change to scan operation

--- src/routes/getRecipients/index.ts
+++ src/routes/getRecipients/index.ts
@@ -1,21 +1,22 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { DatabaseError } from '../../common/errors';
-import { QueryCommand } from '@aws-sdk/client-dynamodb';
-import { dynamoClient, logger } from '../../common/util';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { logger } from '../../common/util';
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler: APIGatewayProxyHandler = async () => {
   try {
     const params = {
       TableName: 'OneHundredLettersPersonTable',
       IndexName: 'LastNameIndex',
-      KeyConditionExpression: 'PK = :pkValue',
-      ExpressionAttributeValues: {
-        ':pkValue': { S: '' },
-      },
       ScanIndexForward: true,
     };
 
-    const result = await dynamoClient.send(new QueryCommand(params));
+    const command = new ScanCommand(params);
+    const result = await docClient.send(command);
 
     return {
       statusCode: 200,
@@ -24,7 +25,7 @@ export const handler: APIGatewayProxyHandler = async () => {
       }),
     };
   } catch (error) {
-    logger.error('Error fetching from DynamoDB: ', error);
+    logger.error('Error scanning from DynamoDB: ', error);
     const dbError = new DatabaseError('Internal Server Error');
     return {
       statusCode: dbError.statusCode,
