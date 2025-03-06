0fb48b8fb6397d8d0a23a5e7b2ad97fbe8739c71
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-03-06 14:35:22 -0800

    fix: try again

--- src/routes/getRecipients/index.ts
+++ src/routes/getRecipients/index.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { DatabaseError } from '../../common/errors';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb';
 import { logger } from '../../common/util';
 
 const client = new DynamoDBClient({});
@@ -11,11 +11,11 @@ export const handler: APIGatewayProxyHandler = async () => {
   try {
     const params = {
       TableName: 'OneHundredLettersPersonTable',
-      IndexName: 'LastNameIndex',
-      ScanIndexForward: true,
+      IndexName: 'LastNameIndex', // Ensure it's using the correct index
+      ScanIndexForward: true, // true for ascending order by `lastName`
     };
 
-    const command = new ScanCommand(params);
+    const command = new QueryCommand(params);
     const result = await docClient.send(command);
 
     return {
@@ -25,7 +25,7 @@ export const handler: APIGatewayProxyHandler = async () => {
       }),
     };
   } catch (error) {
-    logger.error('Error scanning from DynamoDB: ', error);
+    logger.error('Error querying DynamoDB: ', error);
     const dbError = new DatabaseError('Internal Server Error');
     return {
       statusCode: dbError.statusCode,
