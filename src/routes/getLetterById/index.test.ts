a90e465779782bf0027d8bb0b677f91ec891fa28
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-03-10 11:10:07 -0700

    feat: add get letter by ID route

--- src/routes/getLetterById/index.test.ts
+++ src/routes/getLetterById/index.test.ts
@@ -3,22 +3,142 @@ import {
   APIGatewayProxyResult,
   Context,
 } from 'aws-lambda';
+import { dynamoClient, logger } from '../../common/util';
 import { handler } from './index';
 
-describe('getLetterById Route', () => {
-  it('Should return 200 with message.', async () => {
-    const event: APIGatewayProxyEvent = {} as APIGatewayProxyEvent;
+jest.mock('../../common/util', () => ({
+  dynamoClient: {
+    send: jest.fn(),
+  },
+  logger: {
+    error: jest.fn(),
+    info: jest.fn(),
+  },
+}));
+
+describe('getLetterById', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return the letter when found', async () => {
+    const mockLetter = { id: '1', lastName: 'Xavier' };
+    (dynamoClient.send as jest.Mock).mockResolvedValueOnce({
+      Item: mockLetter,
+    });
     const context: Context = {} as Context;
+    const event: APIGatewayProxyEvent = {
+      body: null,
+      headers: {},
+      httpMethod: 'GET',
+      isBase64Encoded: false,
+      path: `/letter/1`,
+      pathParameters: { id: '1' },
+      queryStringParameters: null,
+      stageVariables: null,
+      requestContext: {} as APIGatewayProxyEvent['requestContext'],
+      resource: '',
+    } as unknown as APIGatewayProxyEvent;
     const result = (await handler(
       event,
       context,
       () => {},
     )) as APIGatewayProxyResult;
     expect(result.statusCode).toBe(200);
-    expect(result.body).toBe(
-      JSON.stringify({
-        message: 'getLetterById',
-      }),
+    expect(JSON.parse(result.body || '').data).toEqual(mockLetter);
+  });
+
+  it('should return a 400 error if no letterId is provided', async () => {
+    const context: Context = {} as Context;
+    const event: APIGatewayProxyEvent = {
+      body: null,
+      headers: {},
+      httpMethod: 'GET',
+      isBase64Encoded: false,
+      path: '/letter', // Path without ID
+      pathParameters: null,
+      queryStringParameters: null,
+      stageVariables: null,
+      requestContext: {} as APIGatewayProxyEvent['requestContext'],
+      resource: '',
+    } as unknown as APIGatewayProxyEvent;
+
+    const result = (await handler(
+      event,
+      context,
+      () => {},
+    )) as APIGatewayProxyResult;
+
+    expect(result.statusCode).toBe(400);
+    expect(JSON.parse(result.body || '').message).toBe(
+      'Letter ID is required!',
+    );
+  });
+
+  it('should return a 404 error if the letter is not found', async () => {
+    const letterId = '1';
+
+    (dynamoClient.send as jest.Mock).mockResolvedValueOnce({});
+
+    const context: Context = {} as Context;
+    const event: APIGatewayProxyEvent = {
+      body: null,
+      headers: {},
+      httpMethod: 'GET',
+      isBase64Encoded: false,
+      path: `/letter/${letterId}`,
+      pathParameters: { id: letterId },
+      queryStringParameters: null,
+      stageVariables: null,
+      requestContext: {} as APIGatewayProxyEvent['requestContext'],
+      resource: '',
+    } as unknown as APIGatewayProxyEvent;
+
+    const result = (await handler(
+      event,
+      context,
+      () => {},
+    )) as APIGatewayProxyResult;
+    const responseBody = JSON.parse(result.body || '');
+    expect(result.statusCode).toBe(404);
+    expect(responseBody.error).toBe('NotFoundError');
+    expect(responseBody.message).toBe(`Letter with ID ${letterId} not found!`);
+  });
+
+  it('should return a 500 error if there is a DynamoDB error', async () => {
+    const errorMessage = 'DynamoDB error occurred';
+
+    (dynamoClient.send as jest.Mock).mockRejectedValueOnce(
+      new Error(errorMessage),
+    );
+
+    const context: Context = {} as Context;
+    const event: APIGatewayProxyEvent = {
+      body: null,
+      headers: {},
+      httpMethod: 'GET',
+      isBase64Encoded: false,
+      path: '/letter/1',
+      pathParameters: { id: '1' },
+      queryStringParameters: null,
+      stageVariables: null,
+      requestContext: {} as APIGatewayProxyEvent['requestContext'],
+      resource: '',
+    } as unknown as APIGatewayProxyEvent;
+
+    const result = (await handler(
+      event,
+      context,
+      () => {},
+    )) as APIGatewayProxyResult;
+
+    const responseBody = JSON.parse(result.body || '');
+    expect(result.statusCode).toBe(500);
+    expect(responseBody.error).toBe('DatabaseError');
+    expect(responseBody.message).toBe('Internal Server Error');
+    expect(logger.error).toHaveBeenCalledWith(
+      'Error fetching letter from DynamoDB: ',
+      expect.any(Error),
     );
   });
 });
--- src/routes/getLetterById/index.ts
+++ src/routes/getLetterById/index.ts
@@ -1,10 +1,42 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import {
+  BadRequestError,
+  DatabaseError,
+  NotFoundError,
+} from '../../common/errors';
+import { GetCommand } from '@aws-sdk/lib-dynamodb';
+import { dynamoClient, logger } from '../../common/util';
 
-export const handler: APIGatewayProxyHandler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'getLetterById',
-    }),
-  };
+export const handler: APIGatewayProxyHandler = async (event) => {
+  try {
+    const letterId = event.pathParameters?.id;
+
+    if (!letterId) {
+      return new BadRequestError('Letter ID is required!').build();
+    }
+
+    const params = {
+      TableName: 'OneHundredLettersLetterTable',
+      Key: {
+        letterId: letterId,
+      },
+    };
+
+    const command = new GetCommand(params);
+    const result = await dynamoClient.send(command);
+
+    if (!result.Item) {
+      return new NotFoundError(`Letter with ID ${letterId} not found!`).build();
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: result.Item,
+      }),
+    };
+  } catch (error) {
+    logger.error('Error fetching letter from DynamoDB: ', error);
+    return new DatabaseError('Internal Server Error').build();
+  }
 };
