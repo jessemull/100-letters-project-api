09d5e9dbc931c6aace98d2d5ec2c4d5935bcdecc
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-03-12 16:08:57 -0700

    feat: check for correspondence on letter create and return 404

--- src/routes/createLetter/index.ts
+++ src/routes/createLetter/index.ts
@@ -1,8 +1,12 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import { BadRequestError, DatabaseError } from '../../common/errors';
-import { Letter } from '../../types';
-import { PutCommand } from '@aws-sdk/lib-dynamodb';
+import {
+  BadRequestError,
+  DatabaseError,
+  NotFoundError,
+} from '../../common/errors';
+import { PutCommand, QueryCommand } from '@aws-sdk/lib-dynamodb';
 import { dynamoClient, logger } from '../../common/util';
+import { Letter } from '../../types';
 import { v4 as uuidv4 } from 'uuid';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -38,6 +42,25 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       return new BadRequestError(
         'Correspondence ID, date, imageURL, method, status, text, title, and type are required.',
       ).build();
+    }
+
+    const checkCorrespondenceParams = {
+      TableName: 'OneHundredLettersCorrespondenceTable',
+      KeyConditionExpression: 'correspondenceId = :correspondenceId',
+      ExpressionAttributeValues: {
+        ':correspondenceId': correspondenceId,
+      },
+    };
+
+    const correspondenceResult = await dynamoClient.send(
+      new QueryCommand(checkCorrespondenceParams),
+    );
+
+    if (
+      !correspondenceResult.Items ||
+      correspondenceResult.Items.length === 0
+    ) {
+      return new NotFoundError('Correspondence ID not found.').build();
     }
 
     const letterId = uuidv4();
