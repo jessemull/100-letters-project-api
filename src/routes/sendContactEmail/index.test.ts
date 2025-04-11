ba3c1b3f0e5478bb0702ecd1af8b3b0136391ef1
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-04-11 10:53:14 -0700

    fix: added captcha support

--- src/routes/sendContactEmail/index.test.ts
+++ src/routes/sendContactEmail/index.test.ts
@@ -6,6 +6,10 @@ import {
 import { handler } from './index';
 import { sesClient, logger } from '../../common/util';
 
+global.fetch = jest.fn().mockResolvedValue({
+  json: async () => ({ success: true }),
+});
+
 jest.mock('../../common/util', () => ({
   sesClient: {
     sendEmail: jest.fn(),
@@ -34,7 +38,9 @@ describe('Contact Form Handler', () => {
         lastName: 'Doe',
         message: 'Hello, I need help!',
       }),
-      headers: {},
+      headers: {
+        'G-Recaptcha-Response': 'mock-token',
+      },
       httpMethod: 'POST',
       isBase64Encoded: false,
       path: '/contact',
@@ -66,7 +72,9 @@ describe('Contact Form Handler', () => {
         email: 'test@example.com',
         firstName: 'John',
       }),
-      headers: {},
+      headers: {
+        'g-recaptcha-response': 'mock-token',
+      },
       httpMethod: 'POST',
       isBase64Encoded: false,
       path: '/contact',
@@ -88,13 +96,15 @@ describe('Contact Form Handler', () => {
     expect(result.statusCode).toBe(400);
     expect(JSON.parse(result.body || '').error).toBe('BadRequestError');
     expect(JSON.parse(result.body || '').message).toBe(
-      'Name, email, and message are required.',
+      'Name, email, message and CAPTCHA are required.',
     );
   });
 
   it('should return a 400 response if the body is missing', async () => {
     const event: APIGatewayProxyEvent = {
-      headers: {},
+      headers: {
+        'g-recaptcha-response': 'mock-token',
+      },
       httpMethod: 'POST',
       isBase64Encoded: false,
       path: '/contact',
@@ -116,7 +126,7 @@ describe('Contact Form Handler', () => {
     expect(result.statusCode).toBe(400);
     expect(JSON.parse(result.body || '').error).toBe('BadRequestError');
     expect(JSON.parse(result.body || '').message).toBe(
-      'Name, email, and message are required.',
+      'Name, email, message and CAPTCHA are required.',
     );
   });
 
@@ -136,7 +146,9 @@ describe('Contact Form Handler', () => {
         lastName: 'Doe',
         message: 'Hello, I need help!',
       }),
-      headers: {},
+      headers: {
+        'g-recaptcha-response': 'mock-token',
+      },
       httpMethod: 'POST',
       isBase64Encoded: false,
       path: '/contact',
@@ -161,6 +173,90 @@ describe('Contact Form Handler', () => {
     expect(responseBody.message).toBe('There was an error sending the email.');
     expect(logger.error).toHaveBeenCalledWith(
       'Error sending email: ',
+      expect.any(Error),
+    );
+  });
+
+  it('should return a 400 response if CAPTCHA verification fails', async () => {
+    (global.fetch as jest.Mock).mockResolvedValueOnce({
+      json: async () => ({ success: false }),
+    });
+
+    const event: APIGatewayProxyEvent = {
+      body: JSON.stringify({
+        email: 'test@example.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        message: 'Test message',
+        captchaToken: 'invalid-token',
+      }),
+      headers: {
+        'g-recaptcha-response': 'mock-token',
+      },
+      httpMethod: 'POST',
+      isBase64Encoded: false,
+      path: '/contact',
+      pathParameters: null,
+      queryStringParameters: null,
+      stageVariables: null,
+      requestContext: {} as APIGatewayProxyEvent['requestContext'],
+      resource: '',
+    } as unknown as APIGatewayProxyEvent;
+
+    const context: Context = {} as Context;
+
+    const result = (await handler(
+      event,
+      context,
+      () => {},
+    )) as APIGatewayProxyResult;
+
+    expect(result.statusCode).toBe(400);
+    expect(JSON.parse(result.body || '').message).toBe(
+      'Invalid CAPTCHA. Please try again.',
+    );
+  });
+
+  it('should return a 500 response if CAPTCHA verification throws an error', async () => {
+    (global.fetch as jest.Mock).mockRejectedValueOnce(
+      new Error('CAPTCHA error'),
+    );
+
+    const event: APIGatewayProxyEvent = {
+      body: JSON.stringify({
+        email: 'test@example.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        message: 'Test message',
+        captchaToken: 'valid-token',
+      }),
+      headers: {
+        'g-recaptcha-response': 'mock-token',
+      },
+      httpMethod: 'POST',
+      isBase64Encoded: false,
+      path: '/contact',
+      pathParameters: null,
+      queryStringParameters: null,
+      stageVariables: null,
+      requestContext: {} as APIGatewayProxyEvent['requestContext'],
+      resource: '',
+    } as unknown as APIGatewayProxyEvent;
+
+    const context: Context = {} as Context;
+
+    const result = (await handler(
+      event,
+      context,
+      () => {},
+    )) as APIGatewayProxyResult;
+
+    expect(result.statusCode).toBe(500);
+    expect(JSON.parse(result.body || '').message).toBe(
+      'There was an error verifying the CAPTCHA.',
+    );
+    expect(logger.error).toHaveBeenCalledWith(
+      'Error verifying CAPTCHA: ',
       expect.any(Error),
     );
   });
